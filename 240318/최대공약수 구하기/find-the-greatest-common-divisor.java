24ff439e8c584a787501b8dbc52b40c43dc807b4
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2024-03-18 16:26:54 +0900

    Title: 최대공약수 구하기, Time: 115ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240318/최대공약수 구하기/find-the-greatest-common-divisor.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        calcGCD(n,m);
+
+    }
+
+    static void calcGCD(int n, int m){
+        for(int i=Math.min(n,m);i>=1;i--){
+            if(Math.max(n,m)%i == 0 && Math.min(n,m)%i==0) { 
+                System.out.println(i);
+                return;
+            }
+        }
+    }
+}
