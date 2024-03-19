6d492e2f58fc43749dfa546d1375b1fb34d556cb
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2024-03-19 16:12:24 +0900

    Title: 함수를 이용한 소수 판별, Time: 108ms, Memory: 8MB, Status: Passed - CodeTree

--- 240319/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
+++ 240319/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -19,9 +19,10 @@ public class Main {
 
     static boolean isPrime(int num){
 
+        if(num == 1) return false;
         if(num == 2) return true;
  
-        for(int i=2;i<num;i++){
+        for(int i=2;i<=Math.sqrt(num);i++){
             if(num%i == 0) return false;
         }
         return true;
