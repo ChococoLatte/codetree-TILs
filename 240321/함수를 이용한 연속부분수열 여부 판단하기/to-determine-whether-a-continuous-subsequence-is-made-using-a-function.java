5224e70c9d2792e76283e444d58abe4ac06193df
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2024-03-21 13:57:40 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 84ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240321/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n1 = Integer.parseInt(st.nextToken());
+        int n2 = Integer.parseInt(st.nextToken());
+
+        int[] n1Arr = new int[n1];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n1;i++){
+            n1Arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] n2Arr = new int[n2];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<n2;i++){
+            n2Arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if(isConSecutiveNumberSub(n1Arr, n2Arr)) System.out.println("Yes");
+        else System.out.println("No");
+
+    }
+
+    static boolean isConSecutiveNumberSub(int[] n1Arr, int[] n2Arr){
+
+        for(int i=0;i<n1Arr.length;i++){
+            if(n2Arr[0] == n1Arr[i]){
+                for(int j=0;j<n2Arr.length;j++){
+                    if(n2Arr[j] != n1Arr[i+j]) return false;
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+}
