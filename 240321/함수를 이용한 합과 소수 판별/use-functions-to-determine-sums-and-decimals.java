4e0bd2be90665fcd65b47ada387f54abe8d56969
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2024-03-21 13:27:56 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 112ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240321/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        int cnt = 0;
+        for(int i=a;i<=b;i++){
+            if(isPrime(i) && isEven(i)) cnt++;
+        }
+
+        System.out.println(cnt);
+    }
+
+    static boolean isPrime(int num){
+
+        if(num == 1) return false;
+        if(num == 2) return true;
+
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i == 0) return false;
+        }
+
+        return true;
+    }
+
+    static boolean isEven(int num){
+        String strNum = String.valueOf(num);
+
+        int sum = 0;
+        for(int i=0;i<strNum.length();i++){
+            sum+=strNum.charAt(i)-'0';
+        }
+
+        if(sum%2 == 0) return true;
+        else return false;
+    }
+}
