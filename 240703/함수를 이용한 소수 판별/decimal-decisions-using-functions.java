361ddd230bcb1c6e63c1dd60aead3db490cf8ec5
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2024-07-03 22:08:58 +0900

    Title: 함수를 이용한 소수 판별, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240703/함수를 이용한 소수 판별/decimal-decisions-using-functions.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
+
+       int a = Integer.parseInt(st.nextToken());
+       int b = Integer.parseInt(st.nextToken());
+
+       int tot = 0;
+       for(int i=a;i<=b;i++){
+          if(isPrime(i)) tot+=i;
+       }
+
+       System.out.println(tot);
+    }
+
+    static boolean isPrime(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0) return false;
+        }
+
+        return true;
+    }
+}
