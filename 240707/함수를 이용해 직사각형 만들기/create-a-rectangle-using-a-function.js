928260a7f032bf1be2e3c7a394ab28f383847983
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2024-07-07 19:43:32 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 236ms, Memory: 17MB, Status: Passed - CodeTree

---
+++ 240707/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.js
@@ -0,0 +1,14 @@
+const printRect = (n,m)=>{
+    for(let i=0;i<n;i++){
+        let s = '';
+        for(let j=0;j<m;j++){
+           s+='1';
+        }
+        console.log(s);
+    }
+};
+
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split(" ");
+let [n,m] = input.map(Number);
+printRect(n,m);
