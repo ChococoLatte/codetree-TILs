0debdf3c4cb5468af86e1d7dcabd7951022f0e27
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-11-24 20:33:08 +0900

    Title: 최고의 33위치, Time: 88ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241124/최고의 33위치/best-place-of-33.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+
+        int max = Integer.MIN_VALUE;
+        for(int i=0;i<n-3;i++){
+            for(int j=0;j<n-3;j++){
+                int cnt = 0;
+                for(int k=0;k<3;k++){
+                    for(int l=0;l<3;l++){
+                        cnt+=arr[i+k][j+l];
+                    }
+                }
+                max = Math.max(max,cnt);
+            }
+        }
+
+        System.out.println(max);
+    }
+}
