f075286df56683d67bdf3cf34c9c66d9be0d012c
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-11-24 20:37:44 +0900

    Title: 최고의 33위치, Time: 83ms, Memory: 9MB, Status: Passed - CodeTree

--- 241124/최고의 33위치/best-place-of-33.java
+++ 241124/최고의 33위치/best-place-of-33.java
@@ -7,10 +7,16 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[][] arr = new int[n][n];
+        for(int i=0;i<n;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
         int max = Integer.MIN_VALUE;
-        for(int i=0;i<n-3;i++){
-            for(int j=0;j<n-3;j++){
+        for(int i=0;i<=n-3;i++){
+            for(int j=0;j<=n-3;j++){
                 int cnt = 0;
                 for(int k=0;k<3;k++){
                     for(int l=0;l<3;l++){
@@ -20,7 +26,6 @@ public class Main {
                 max = Math.max(max,cnt);
             }
         }
-
         System.out.println(max);
     }
 }
