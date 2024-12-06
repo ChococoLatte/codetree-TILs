33505880fb24c680aa9124f63741c44cbfb8faab
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-12-06 11:39:36 +0900

    Title: 양수 직사각형의 최대 크기, Time: 94ms, Memory: 9MB, Status: Passed - CodeTree

--- 241206/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
+++ 241206/양수 직사각형의 최대 크기/max-area-of-positive-rectangle.java
@@ -2,11 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n,m,max;
+    static int n, m, max;
     static int[][] arr;
-    static boolean[][] visited;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,40 +13,37 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
 
         arr = new int[n][m];
-        
-        for(int i=0;i<n;i++){
+
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<m;j++){
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        max = -1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                for(int k=i;k<n;k++){
-                    for(int l=j;l<m;l++){
-                        visited = new boolean[n][m];
-                        max = Math.max(max,calc(i,j,k,l));
+        max = -1; // 모든 값이 음수인 경우의 기본값
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = i; k < n; k++) {
+                    for (int l = j; l < m; l++) {
+                        if (isPositiveRectangle(i, j, k, l)) {
+                            max = Math.max(max, (k - i + 1) * (l - j + 1));
+                        }
                     }
                 }
             }
         }
-        
+
         System.out.println(max);
     }
 
-    static int calc(int x,int y,int gx,int gy){
-        
-        int cnt = 0;
-        for(int i=x;i<=gx;i++){
-            for(int j=y;j<=gy;j++){
-                if(arr[i][j]<0) return -1;
-                else cnt++;
+    // 주어진 직사각형이 양수 직사각형인지 확인
+    static boolean isPositiveRectangle(int x1, int y1, int x2, int y2) {
+        for (int i = x1; i <= x2; i++) {
+            for (int j = y1; j <= y2; j++) {
+                if (arr[i][j] <= 0) return false; // 음수를 발견하면 즉시 종료
             }
         }
-
-        return cnt;
+        return true;
     }
-
 }
