47a7168a9fbf762522a7c8d4fde7439506215c4d
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-12-09 14:59:41 +0900

    Title: 삼각형 컨베이어 벨트, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241209/삼각형 컨베이어 벨트/conveyor-belt-triangle.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[3][n];
+        for(int i=0;i<3;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        while(t-->0){
+            int temp1 = arr[0][n-1];
+            int temp2 = arr[1][n-1];
+            int temp3 = arr[2][n-1];
+
+            for(int i=0;i<3;i++){
+                for(int j=n-1;j>=1;j--){
+                    arr[i][j] = arr[i][j-1];
+                }
+            }
+
+            arr[0][0] = temp3;
+            arr[1][0] = temp1;
+            arr[2][0] = temp2; 
+        }
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<n;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
