4d736bd124ba0497d809d8f66fae13a6f170650c
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-12-09 14:42:26 +0900

    Title: 컨베이어 벨트, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241209/컨베이어 벨트/conveyor-belt.java
+++ 241209/컨베이어 벨트/conveyor-belt.java
@@ -13,32 +13,25 @@ public class Main {
         for(int i=0;i<2;i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0;j<n;j++){
-                if(i==0){
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                }else{
-                    arr[i][n-1-j] = Integer.parseInt(st.nextToken());
-                }
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        for(int i=0;i<t;i++){
-            int temp1 = arr[1][0];
-            int temp2 = arr[0][n-1];
-            for(int j=n-1;j>=1;j--){
-                arr[0][j] = arr[0][j-1];
+        while(t-->0){
+            int temp1 = arr[0][n-1];
+            int temp2 = arr[1][n-1];
+            for(int i=0;i<2;i++){
+                for(int j=n-1;j>=1;j--){
+                    arr[i][j] = arr[i][j-1];
+                }
             }
-
-            for(int j=0;j<n-1;j++){
-                arr[1][j] = arr[1][j+1];
-            }
-            arr[0][0] = temp1;
-            arr[1][n-1] = temp2;
+            arr[0][0] = temp2;
+            arr[1][0] = temp1;
         }
 
         for(int i=0;i<2;i++){
             for(int j=0;j<n;j++){
-                if(i == 0) System.out.print(arr[i][j]+" ");
-                else System.out.print(arr[i][n-1-j]+" ");
+                System.out.print(arr[i][j]+" ");
             }
             System.out.println();
         }
