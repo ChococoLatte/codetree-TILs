240bb4ed1f52dd9b3a86dedb7f0f61c023e8d06b
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-12-23 10:28:44 +0900

    Title: 기울어진 직사각형의 회전, Time: 197ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241223/기울어진 직사각형의 회전/rotate-slanted-rectangle.java
@@ -0,0 +1,76 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[][] arr;
+    static int[] dx = {-1,-1,1,1};
+    static int[] dy = {1,-1,-1,1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken())-1;
+        int c = Integer.parseInt(st.nextToken())-1;
+        int m1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int m3 = Integer.parseInt(st.nextToken());
+        int m4 = Integer.parseInt(st.nextToken());
+        int dir = Integer.parseInt(st.nextToken());
+
+        int[] mList = {m1,m2,m3,m4};
+        rotate(r,c,mList,dir);
+
+
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void rotate(int r, int c, int[] mList, int dir){
+
+        int r1 = r;
+        int c1 = c;
+
+        LinkedList<Integer> list = new LinkedList<>();
+        for(int i=0;i<4;i++){
+            for(int j=0;j<mList[i];j++){
+                r1+=dx[i];
+                c1+=dy[i];
+
+                list.add(arr[r1][c1]);
+            }
+        }
+
+        if(dir == 1){
+            int val = list.pollFirst();
+            list.addLast(val);
+        }else if(dir == 0){
+            int val = list.pollLast();
+            list.addFirst(val);
+        }
+
+        for(int i=0;i<4;i++){
+            for(int j=0;j<mList[i];j++){
+                r+=dx[i];
+                c+=dy[i];
+
+                arr[r][c] = list.pollFirst();
+            }
+        }
+    }
+}
