db083d872f6dd39b566de63a08a4c8940505ae24
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-12-23 15:35:10 +0900

    Title: 십자 모양 폭발, Time: 89ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241223/십자 모양 폭발/cross-shape-bomb.java
+++ 241223/십자 모양 폭발/cross-shape-bomb.java
@@ -24,9 +24,11 @@ public class Main {
         int r = arr[x][y];
         arr[x][y] = 0;
         for(int i = 0; i < 4; i++) {
-            for(int j = 0; j < r; j++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            int nx = x;
+            int ny = y;
+            for(int j = 1; j < r; j++) {
+                nx+=dx[i];
+                ny+=dy[i];
 
                 if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
                 arr[nx][ny] = 0;
