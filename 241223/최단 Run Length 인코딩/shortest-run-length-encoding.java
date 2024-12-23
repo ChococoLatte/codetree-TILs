5461f5be54e42f92f7d2869c392e2f0a21fdb8d1
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2024-12-23 13:43:08 +0900

    Title: 최단 Run Length 인코딩, Time: 92ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 241223/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 241223/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,44 +6,32 @@ public class Main {
 
         String s = br.readLine();
         int min = s.length();
-        for(int i = 0; i < s.length(); i++){
-            if(i!=0) s = rotate(s);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i != 0) s = rotate(s); // 오른쪽으로 shift
 
             char fValue = s.charAt(0);
             int cnt = 1;
+            int encodedLength = 0;
 
-            String answer = "";
-            for(int j=1;j<s.length();j++){
-                if(fValue == s.charAt(j)){
-                    cnt++;
-                }else if(fValue != s.charAt(j)){
-                    answer+=(fValue+Integer.toString(cnt));
+            for (int j = 1; j < s.length(); j++) {
+                if (fValue != s.charAt(j)) {
+                    encodedLength += 1 + String.valueOf(cnt).length(); // 문자 + 숫자 길이
                     fValue = s.charAt(j);
                     cnt = 1;
-                }
-
-                if(j == s.length()-1){
-                    answer+=(fValue+Integer.toString(cnt));
+                } else {
+                    cnt++;
                 }
             }
-            min = Math.min(min,answer.length());
+            // 마지막 문자 처리
+            encodedLength += 1 + String.valueOf(cnt).length();
+
+            min = Math.min(min, encodedLength);
         }
         System.out.println(min);
     }
 
-    static String rotate(String s){
-
-        LinkedList<Character> list = new LinkedList<>();
-        for(char c: s.toCharArray()){
-            list.add(c);
-        }
-
-        list.addFirst(list.pollLast());
-        StringBuilder sb = new StringBuilder();
-        for(char c: list){
-            sb.append(c);
-        }
-
-        return sb.toString();
+    static String rotate(String s) {
+        return s.charAt(s.length() - 1) + s.substring(0, s.length() - 1);
     }
 }
