28c9351cf8bc172b14686c7c4ed0c8b48dce097d
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2025-01-01 20:40:22 +0900

    Title: 1차원 폭발 게임, Time: 86ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250101/1차원 폭발 게임/The-1D-bomb-game.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n,m;
+    static boolean isFlag;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(Integer.parseInt(br.readLine()));
+        }
+
+        while(true){
+            isFlag = false;
+            if(!list.isEmpty()) removeBomb(list);
+
+            if(!isFlag || list.isEmpty()) break;
+        }
+
+        System.out.println(list.size());
+        for(int i=0;i<list.size();i++){
+            System.out.println(list.get(i));
+        }
+    }
+
+    static void removeBomb(ArrayList<Integer> list){
+        if (list.isEmpty()) return;
+ 
+        int firstIdx = 0;
+        int first = list.get(0);
+        int cnt = 1;
+        
+        for(int i=1;i<list.size();i++){
+            if(first == list.get(i)){
+                cnt++;
+                if(cnt>=m){
+                    for(int j=0;j<cnt;j++){
+                        list.remove(firstIdx);
+                    }
+                    isFlag = true;
+                    break;
+                }
+            }else{
+                firstIdx = i;
+                first = list.get(i);
+                cnt = 1;
+            }
+
+            if(i == list.size()-1){
+                if(cnt>=m){
+                    for(int j=0;j<cnt;j++){
+                        list.remove(firstIdx);
+                    }
+                    isFlag = true;
+                    break;
+                }
+            }
+        }
+    }
+}
