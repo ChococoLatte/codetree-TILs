246bd3ee587be49d51fbd97cb2b14879155c4810
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2025-01-05 20:39:17 +0900

    Title: 단 한 번의 2048 시도, Time: 84ms, Memory: 9MB, Status: Passed - CodeTree

--- 250105/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 250105/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -3,88 +3,179 @@ import java.util.*;
 
 public class Main {
     static int[][] arr;
+
+    // 0을 오른쪽으로 밀기
+    public static void shiftRight(int[] row) {
+        int[] newRow = new int[4];
+        int idx = 3;
+
+        // 숫자들을 오른쪽으로 밀기
+        for (int i = 3; i >= 0; i--) {
+            if (row[i] != 0) {
+                newRow[idx--] = row[i];
+            }
+        }
+
+        // 새로운 배열을 기존 배열에 덮어 씌우기
+        for (int i = 0; i < 4; i++) {
+            row[i] = newRow[i];
+        }
+    }
+
+    // 0을 왼쪽으로 밀기
+    public static void shiftLeft(int[] row) {
+        int[] newRow = new int[4];
+        int idx = 0;
+
+        // 숫자들을 왼쪽으로 밀기
+        for (int i = 0; i < 4; i++) {
+            if (row[i] != 0) {
+                newRow[idx++] = row[i];
+            }
+        }
+
+        // 새로운 배열을 기존 배열에 덮어 씌우기
+        for (int i = 0; i < 4; i++) {
+            row[i] = newRow[i];
+        }
+    }
+
+    // 0을 위로 밀기
+    public static void shiftUp() {
+        for (int i = 0; i < 4; i++) {
+            int[] col = new int[4];
+            for (int j = 0; j < 4; j++) {
+                col[j] = arr[j][i];
+            }
+
+            shiftLeft(col);
+
+            for (int j = 0; j < 4; j++) {
+                arr[j][i] = col[j];
+            }
+        }
+    }
+
+    // 0을 아래로 밀기
+    public static void shiftDown() {
+        for (int i = 0; i < 4; i++) {
+            int[] col = new int[4];
+            for (int j = 0; j < 4; j++) {
+                col[j] = arr[j][i];
+            }
+
+            shiftRight(col);
+
+            for (int j = 0; j < 4; j++) {
+                arr[j][i] = col[j];
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         arr = new int[4][4];
-        for(int i=0;i<4;i++){
+        for (int i = 0; i < 4; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j=0;j<4;j++){
+            for (int j = 0; j < 4; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         char c = br.readLine().charAt(0);
-        if(c == 'R'){
-            for(int i=0;i<4;i++){
-                for(int j=3;j>=1;j--){
-                    if(arr[i][j] == 0){
-                        for(int k=j;k>=1;k--){
-                            arr[i][k] = arr[i][k-1];
-                            arr[i][k-1] = 0;
-                        }
-                    }
+        if (c == 'R') {
+            // 숫자 합치기
+            for (int i = 0; i < 4; i++) {
+                // 오른쪽으로 밀기
+                shiftRight(arr[i]);
 
-                    if(arr[i][j] == arr[i][j-1]){
-                        arr[i][j]+=arr[i][j-1];
-                        arr[i][j-1] = 0;
+                // 합치기
+                for (int j = 3; j > 0; j--) {
+                    if (arr[i][j] == arr[i][j - 1]) {
+                        arr[i][j] *= 2;
+                        arr[i][j - 1] = 0;
                     }
                 }
-            }
-        }else if(c == 'L'){
-            for(int i=0;i<4;i++){
-                for(int j=0;j<3;j++){
-                    if(arr[i][j] == 0){
-                        for(int k=j;k<3;k++){
-                            arr[i][k] = arr[i][k+1];
-                            arr[i][k+1] = 0;
-                        }
-                    }
 
-                    if(arr[i][j] == arr[i][j+1]){
-                        arr[i][j]+=arr[i][j+1];
-                        arr[i][j+1] = 0;
+                // 다시 밀기
+                shiftRight(arr[i]);
+            }
+        } else if (c == 'L') {
+            for (int i = 0; i < 4; i++) {
+                // 왼쪽으로 밀기
+                shiftLeft(arr[i]);
+
+                // 합치기
+                for (int j = 0; j < 3; j++) {
+                    if (arr[i][j] == arr[i][j + 1]) {
+                        arr[i][j] *= 2;
+                        arr[i][j + 1] = 0;
                     }
                 }
-            }
-        }else if(c=='U'){
-            for(int i=0;i<4;i++){
-                for(int j=0;j<3;j++){
-                    if(arr[j][i] == 0){
-                        for(int k=j;k<3;k++){
-                            arr[k][i] = arr[k+1][i];
-                            arr[k+1][i] = 0;
-                        }
-                    }
 
-                    if(arr[j][i] == arr[j+1][i]){
-                        arr[j][i]+=arr[j+1][i];
-                        arr[j+1][i] = 0;
+                // 다시 밀기
+                shiftLeft(arr[i]);
+            }
+        } else if (c == 'U') {
+            for (int i = 0; i < 4; i++) {
+                // 위로 밀기
+                int[] col = new int[4];
+                for (int j = 0; j < 4; j++) {
+                    col[j] = arr[j][i];
+                }
+
+                shiftLeft(col);
+
+                // 합치기
+                for (int j = 0; j < 3; j++) {
+                    if (col[j] == col[j + 1]) {
+                        col[j] *= 2;
+                        col[j + 1] = 0;
                     }
                 }
-            }
-        }else{
-            for(int i=0;i<4;i++){
-                for(int j=3;j>=1;j--){
-                    if(arr[j][i] == 0){
-                       for(int k=j;k>=1;k--){
-                            arr[k][i] = arr[k-1][i];
-                            arr[k-1][i] = 0;
-                        }
-                    }
 
-                    if(arr[j][i] == arr[j-1][i]){
-                        arr[j][i]+=arr[j-1][i];
-                        arr[j-1][i] = 0;
+                // 다시 밀기
+                shiftLeft(col);
+
+                // 업데이트된 컬럼을 다시 배열에 반영
+                for (int j = 0; j < 4; j++) {
+                    arr[j][i] = col[j];
+                }
+            }
+        } else {
+            for (int i = 0; i < 4; i++) {
+                // 아래로 밀기
+                int[] col = new int[4];
+                for (int j = 0; j < 4; j++) {
+                    col[j] = arr[j][i];
+                }
+
+                shiftRight(col);
+
+                // 합치기
+                for (int j = 3; j > 0; j--) {
+                    if (col[j] == col[j - 1]) {
+                        col[j] *= 2;
+                        col[j - 1] = 0;
                     }
+                }
+
+                // 다시 밀기
+                shiftRight(col);
+
+                // 업데이트된 컬럼을 다시 배열에 반영
+                for (int j = 0; j < 4; j++) {
+                    arr[j][i] = col[j];
                 }
             }
         }
 
+        // 출력
         StringBuilder sb = new StringBuilder();
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
                 sb.append(arr[i][j]).append(" ");
             }
             sb.append("\n");
