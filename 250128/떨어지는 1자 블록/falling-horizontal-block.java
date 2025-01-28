5d4be9326ee9b29390f7d623b5bb766cc09ab169
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2025-01-28 14:21:23 +0700

    Title: 떨어지는 1자 블록, Time: 91ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250128/떨어지는 1자 블록/falling-horizontal-block.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[n][n];
+        for(int i=0;i<n;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<n;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int maxLine = 0;
+        for(int i=0;i<n;i++){
+            boolean isEmpty = true;
+            for(int j=k-1;j<k+m-1;j++){
+                if(arr[i][j]!=0){
+                    isEmpty = false;
+                    break;
+                }
+            }
+            if(isEmpty){
+                maxLine = i;
+            }else{
+                for(int j=k-1;j<k+m-1;j++){
+                    arr[maxLine][j] = 1;    
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                sb.append(arr[i][j]+" ");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
