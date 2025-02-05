0c787ea69c034002fcc5eb0ebdc5960ef69d6a0b
ChococoLatte <62333600+ChococoLatte@users.noreply.github.com>
2025-02-05 19:04:52 +0700

    Title: 1차원 윷놀이, Time: 109ms, Memory: 10MB, Status: Passed - CodeTree

--- 250205/1차원 윷놀이/yutnori-1d.java
+++ 250205/1차원 윷놀이/yutnori-1d.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class Main {
     static int N,M,K,max;
     static int[] turns;
-    static int[] num;
-    public static void main(String[] args) throws IOException {
+    static int[] nums;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
@@ -13,49 +13,46 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        num = new int[N];
+        nums = new int[K+1];
         turns = new int[N];
-
+        
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
             turns[i] = Integer.parseInt(st.nextToken());
         }
 
         max = 0;
-        perm(0);
+        dfs(0);
 
         System.out.println(max);
 
     }
 
-    static void perm(int cnt){
+    static void dfs(int cnt){
+
+        max = Math.max(max,calc());
 
         if(cnt == N){
-            calcMax();
             return;
         }
 
         for(int i=1;i<=K;i++){
-            num[cnt] = i;
-            perm(cnt+1);
+
+            if(nums[i]>=M-1) continue;
+
+            nums[i]+=turns[cnt];
+            dfs(cnt+1);
+            nums[i]-=turns[cnt];
         }
     }
 
-    static void calcMax(){
-        
+    static int calc(){
+
         int cnt = 0;
-
-        int[] arr = new int[K+1];
-        for(int i=0;i<N;i++){
-            arr[num[i]] += turns[i];
-        }
-
         for(int i=1;i<=K;i++){
-            if(arr[i]>=M-1){
-                cnt++;
-            }
+            if(nums[i]>=M-1) cnt++;
         }
 
-        max = Math.max(max,cnt);
+        return cnt;
     }
 }
